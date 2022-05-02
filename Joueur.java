23a61bb2257bfc3d62669d2afb7940eee1723881
Rocketnseven <104773859+Rocketnseven@users.noreply.github.com>
2022-05-02 15:48:54 +0200

    Joueur.java
    it's not yet an abstract

---
+++ Joueur.java
@@ -0,0 +1,56 @@
+public class Joueur{
+
+
+	public String nom;
+	public String prenom;
+  public int id;
+	
+	public Personne() {
+		this.nom = "";
+		this.prenom = "";
+    this.id = 0;
+	}
+	
+	public Personne(String nom, String prenom, int id) {
+		this.nom = nom;
+		this.prenom = prenom;
+    this.id = id;
+	}
+	
+	public String getNom() {
+		return this.nom;
+	}
+	
+	public String getPrenom() {
+		return this.prenom;
+	}
+  
+  public Int getId() {
+    return this.id;
+  }
+	
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+	
+	public void setPrenom(String prenom) {
+		this.prenom = prenom;
+	}
+	
+  public void setId(int id) {
+    this.id = id
+	
+    	public String toString() {
+		return prenom + "\t" + nom;
+	}
+
+	public boolean equals(Object o) {
+		if (o == null || o.getClass() != this.getClass()) {
+			return false;
+		}
+		
+		Personne p = (Personne) o;
+		return p.getNom().equals(this.getNom()) && p.getPrenom().equals(this.getPrenom());
+	}
+	
+}
