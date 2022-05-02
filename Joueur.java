da873ed09e2c32ef9fe6c31046ec1c510ea4b604
LucasTaucoory <lucastaucoory@gmail.com>
2022-05-02 16:17:41 +0200

    test les copains

--- Joueur.java
+++ Joueur.java
@@ -1,45 +1,47 @@
 public class Joueur{
 
+
+	//Attributs
 	private String nom;
 	private String prenom;
 	private int id;
-	
+
 	public Personne() {
 		this.nom = "";
 		this.prenom = "";
     		this.id = 0;
 	}
-	
+
 	public Personne(String nom, String prenom, int id) {
 		this.nom = nom;
 		this.prenom = prenom;
     		this.id = id;
 	}
-	
+
 	public String getNom() {
 		return this.nom;
 	}
-	
+
 	public String getPrenom() {
 		return this.prenom;
 	}
-  
+
  	 public Int getId() {
     		return this.id;
  	}
-	
+
 	public void setNom(String nom) {
 		this.nom = nom;
 	}
-	
+
 	public void setPrenom(String prenom) {
 		this.prenom = prenom;
 	}
-	
+
   	public void setId(int id) {
    		 this.id = id
 	}
-	
+
     	public String toString() {
 		return prenom + "\t" + nom;
 	}
@@ -48,9 +50,9 @@ public class Joueur{
 		if (o == null || o.getClass() != this.getClass()) {
 			return false;
 		}
-		
+
 		Personne p = (Personne) o;
 		return p.getNom().equals(this.getNom()) && p.getPrenom().equals(this.getPrenom());
 	}
-	
+
 }
